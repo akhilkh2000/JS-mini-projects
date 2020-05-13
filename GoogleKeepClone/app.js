2f9375ba789f6d619de493d709a31613ea20f759
akhil <akhilkhubchandani@gmail.com>
2020-05-13 21:11:06 +0530

    fixed events and close form

--- GoogleKeepClone/app.js
+++ GoogleKeepClone/app.js
@@ -6,9 +6,12 @@ class App{
 
         //DOM - elements
         this.$form = document.querySelector('#form'); // we use $ to differentiate html elements as compared to data
+        this.$notes = document.querySelector('#notes');
         this.$noteTitle = document.querySelector('#note-title');
         this.$noteText =  document.querySelector('#note-text');
         this.$formButtons = document.querySelector('#form-buttons');
+        this.$formCloseButton = document.querySelector("#form-close-button");
+        this.$placeHolder = document.querySelector('#placeholder');
 
         this.addEventListeners(); // to make sure all events are added when app starts up
     }
@@ -34,14 +37,29 @@ class App{
 
 
         });
+
+        // for close event
+        this.$formCloseButton.addEventListener('click',event =>{
+            event.stopPropagation(); // so that it doesnt trigger our click event on body 
+            this.closeForm();
+            // console.log("closed");
+        });
+
     }
 
     handleFormClick(event){
         const isFormClicked  = this.$form.contains(event.target);
+        const title = this.$noteTitle.value;
+        const text = this.$noteText.value;
+        const hasNote =  title || text;
         if(isFormClicked){
             // open the form
             this.openForm();
-        } else{
+        } else if(hasNote){
+            // that means the user clicks away from the form and the form has data so we have to save the note
+            this.addNote({title,text});
+        }
+        else{
             //close form
             this.closeForm();
         }
@@ -57,6 +75,9 @@ class App{
         this.$form.classList.remove('form-open');
         this.$noteTitle.style.display = 'none'; 
         this.$formButtons.style.display = 'none';
+        // to clear inputs when form is collapsed
+        this.$noteTitle.value = '';
+        this.$noteText.value = '';
     }
 
     addNote(note) {
@@ -65,14 +86,43 @@ class App{
              title : note.title,
              text : note.text ,
              // if notes exist then we take id of last note and add 1
-             id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+             id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
+             color:"white"
 
          };
         this.notes = [...this.notes , newNote]; //using spread operator
-         console.log(this.notes);
-
+          console.log(this.notes);
+         this.displayNotes();
+         this.closeForm();
     }
 
+    displayNotes(){
+            const hasNotes = this.notes.length > 0;
+            // if we have notes in our array we hide our placeholder text
+            if(hasNotes){
+                this.$placeHolder.style.display = "none";
+            } else{
+                this.$placeHolder.style.display = "flex";
+            }
+
+           this.$notes.innerHTML = this.notes.map(note => `
+             <div style ="background : ${note.color};" class = "note">
+                <div class = "${note.title && 'note-title'}">${note.title}</div>
+                <div class ="note-text">${note.text}</div>
+                <div class = "toolbar-container">
+                    <div class = "toolbar">
+                    <img class = "toolbar-color" src ="https://icon.now.sh/palette">
+                    <img class = "toolbar-delete" src ="https://icon.now.sh/delete">
+                        
+                    </div>
+                </div>
+             </div>
+            `) .join("");
+
+            //.join to join everything into an empty string is used to get rid of the commasbetween array elements that is shown on the webpage if we don't use it
+
+    }
+   
 }
 
 new App();
