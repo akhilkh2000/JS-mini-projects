54da6396aa87db0cb5c38e6935466c2df0321ba8
akhil <akhilkhubchandani@gmail.com>
2020-06-03 15:53:51 +0530

    fetching from API endpoint

--- HackerNewsClone/pages/stories.js
+++ HackerNewsClone/pages/stories.js
@@ -1,6 +1,42 @@
 // this file gives the display content for each route
 import view from '../utils/view.js';
 
-export default function stories(path){
-    view.innerHTML = `<div> ${path} </div>`;
+export default async function stories(path){
+    const stories = await getStories(path);
+     
+    //console.log(stories);
+    const hasStories = stories.length > 0;
+  
+    let story = '';
+    if(hasStories){
+         story = (stories.map(s => JSON.stringify(s)));
+    } else{
+         story = 'No stories';
+    }
+    view.innerHTML = `<div>${story}</div>`;
 }
+
+async function getStories(path){
+    // the first two routes in our app are different from the API endpoint routes so we need to match them
+    const isHomeRoute = (path === '/');
+    const isNewRoute = (path ==='/new');
+    if(isHomeRoute){
+        path = '/news';
+    }else if(isNewRoute){
+        path = '/newest';
+    }
+
+   const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
+    const stories = await response.json();
+    return stories;
+}
+
+
+//API Endpoint
+// https://node-hnapi.herokuapp.com
+
+//the routes on the api endpoint are the routes on the right
+// / (Top) -> /new
+//  /new (New) -> /newest
+// /ask (Ask) -> /show
+// /jobs 
